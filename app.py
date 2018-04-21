c5f5b9fa9d735caabe22a0505b18d3fbccbc3c08
Levkovskyi Vlad <levkoovskyi@gmail.com>
2018-04-21 11:47:11 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from aiohttp_session import session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from aiohttp import web
+
+import hashlib
+from motor import motor_asyncio as ma
+
+import settings
+from settings import log
+from routes import routes
+
+
+middle = [
+    session_middleware(EncryptedCookieStorage(hashlib.sha256(bytes(settings.SECRET_KEY, 'utf-8')).digest())),
+]
+
+app = web.Application(middlewares=middle)
+
+# route part
+for route in routes:
+    app.router.add_route(route[0], route[1], route[2], name=route[3])
+app['static_root_url'] = '/static'
+app.router.add_static('/static', 'static', name='static')
+# end route part
+
+# db connect
+app.client = ma.AsyncIOMotorClient(settings.MONGO_HOST)
+app.db = app.client[routes.MONGO_DB_NAME]
+# end db connect
+
+log.debug('start server')
+web.run_app(app)
+log.debug('Stop server end')
